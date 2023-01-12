d2d2c20857e5c8e88e9a5e5d23011a10efe30e8a
Pimanus Kutragoon <pimanus1234@gmail.com>
2023-01-12 17:11:48 +0700

    add codegen and seperate backend

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,4 @@
-export type Message = {
-  body: string;
-};
-export type User = {
-  name: string;
-  messages: Message[];
-};
+import type { GetUsersQuery } from "./graphql/generated";
+
+export type Message = GetUsersQuery["users"][0]["messages"][0];
+export type User = GetUsersQuery["users"][0];
